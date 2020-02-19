28e1ae889304e03578d7086e8499c7675ae687cf
Flavius Bindea <flav@flav.com>
2020-02-20 00:37:37 +0100

    add target

---
+++ target.py
@@ -0,0 +1,72 @@
+import math
+from datetime import datetime
+
+def to_angle(deg, minute):
+  return deg + minute/60.0 if deg >= 0.0 else deg - minute/60.0
+
+def format_lat(deg):
+  return "{:02d}{:06.3f}".format(int(deg), deg%1*60) 
+
+def format_lon(deg):
+  return "{:03d}{:06.3f}".format(int(deg), deg%1*60) 
+
+class Target:
+  def __init__(self, mmsi, lat, lon, course, speed, heading):
+    """
+    Construct a new 'Target' object.
+
+    Parameters
+    ----------
+    mmsi: string
+      mmsi
+    lat: float
+      latitude in degres. Positive north
+    lon: float
+      longitude in degres. Positive east
+    course: float
+      course over ground in degres, used for position update : 0.0 = north, 90.0 = east, 180.0 = south, 270.0 = west
+    heading: float
+      informational only
+    speed: float
+      speed in knots
+    """
+    self.mmsi = mmsi
+    self.lat = lat
+    self.lon = lon
+    self.course = course
+    self.speed = speed
+    self.heading = heading
+    self.datetime = datetime.now()
+  def update(self):
+    """
+    Updates the position since last update
+    uses the real time difference
+    """
+    lat_a = self.lat
+    lon_a = self.lon
+    new_time = datetime.now()
+    dur = (new_time - self.datetime).total_seconds()
+    lat_b = lat_a + (self.speed * (dur/3600.0)
+                      * math.cos(math.radians(self.course))
+                      / 60.0)
+    lat_m = (lat_a + lat_b)/2
+    lon_b = lon_a + (self.speed * (dur/3600.0)
+                      * math.sin(math.radians(self.course))
+                      / math.cos(math.radians(lat_m)) / 60.0)
+    self.lat = lat_b
+    self.lon = lon_b
+    self.datetime = new_time    
+
+def to_angle(deg, minute):
+  return deg + minute/60
+
+def format_lat(deg):
+  return "{:02d}{:06.3f}".format(int(deg), deg%1*60) 
+
+def format_lon(deg):
+  return "{:03d}{:06.3f}".format(int(deg), deg%1*60) 
+
+#print format_lon(to_angle(45, 0.7))
+#print format_lon(to_angle(5, 30.7))
+#print format_lon(to_angle(45, 30.7))
+#print format_lon(to_angle(145, 58.332))
